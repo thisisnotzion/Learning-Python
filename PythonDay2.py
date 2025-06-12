62bbd5210fc7ffa2b08d30f0d88266df873bde92
thisisnotzion <souravsbansode@gmail.com>
2025-06-12 15:16:35 +0530

    string manupulation

---
+++ PythonDay2.py
@@ -0,0 +1,20 @@
+print("hello world")
+score = 0
+score += 1 
+print(score)
+print("your score is " + str(score))
+
+score = 0
+height = 168.0
+is_winning = True
+print(f"{score} {height} {is_winning}")
+
+
+print("Welcome to the rollcoaster!")
+height = input("What is your height?")
+height > 150
+
+if height >= 150:
+    print("you are eligible congrats!")
+else:
+    print("Sorry Grow faster next time")
