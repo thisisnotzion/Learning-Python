3427cb3459e200660674fb158c4ce58f47c181b8
thisisnotzion <souravsbansode@gmail.com>
2025-06-13 13:48:28 +0530

    Nested elif

--- PythonDay2.py
+++ PythonDay2.py
@@ -18,3 +18,46 @@ if height >= 150:
     print("you are eligible congrats!")
 else:
     print("Sorry Grow faster next time")
+
+
+#Nested if / else
+
+print("welcome to the rollcoaster")
+height = int(input("What is your hieght in cm?"))
+
+if hight >= 120:
+    print("congrats you are eligible")
+else:
+    print("Sorry grow faster next time")
+    
+age = int(input("What is you age?"))
+if age <= 18:
+    print("You need to pay $12")
+elif age <= 12:
+    print("you need to pay $5")
+else:
+    print("you need to pay $15")
+
+# Nested if / else
+
+print("welcome to the rollcoaster")
+height = int(input("What is your height in cm?"))
+bill = 0
+
+if height >= 120:
+    print("congrats you are eligible")
+
+age = int(input("What is you age?"))
+if age >= 18:
+    bill = 12
+    print("You need to pay $12")
+elif age <= 12:
+    bill = 5
+    print("you need to pay $5")
+
+wants_photos = input("Do you want to take a photo? type y for yes and n for no")
+if wants_photos == "y":
+    bill += 3
+    print(f"your total bill is ${bill}")
+else:
+    print("Sorry grow faster next time")
